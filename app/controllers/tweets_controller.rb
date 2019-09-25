35e02eb2cec3c5ac9498034406842329f114d282
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-25 23:28:41 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,7 +32,7 @@ class TweetsController < ApplicationController
     if !logged_in?
       redirect to "/login"
     else
-      @tweet = Tweet.find_by(params[:id])
+      @tweet = Tweet.find(params[:id])
       erb :"/tweets/show"
     end
   end
